61673fa7070c4e29a0d66287518606721167348a
NicolMejiaS <nmejia@ardabytec.com>
2021-05-25 12:28:41 -0500

    +SSe agrego el background de la vista help_decition

--- app/src/main/java/com/temi/app2/MainActivity.java
+++ app/src/main/java/com/temi/app2/MainActivity.java
@@ -5,10 +5,13 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -18,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
     TTSManager ttsManager = null;
     Movimiento movimiento = null;
     Bateria bateria = null;
-    private ImageButton btnHelp;
+    //private ImageButton btnHelp;
+    private Button btnHelp;
+    private VideoView vV;
     private static final String[] PERMISSIONS_STORAGE = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
@@ -36,12 +41,21 @@ public class MainActivity extends AppCompatActivity {
         movimiento = new Movimiento(this,MainActivity.this,ttsManager);
         bateria = new Bateria(ttsManager,movimiento,this,MainActivity.this);
         if(!bateria.EsBateriaBaja()){
-            btnHelp = findViewById(R.id.help);
+            vV = findViewById(R.id.vV);
+            vV.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.maps));
+            vV.start();
+            btnHelp = findViewById(R.id.btnHelp);
             btnHelp.setOnClickListener(v -> {
                 ttsManager.initQueue("Buen día ¿En qué le puedo ayudar?");
                 Intent sig = new Intent(MainActivity.this, Option_Accion.class);
                 startActivity(sig);
             });
+            //btnHelp = findViewById(R.id.help);
+            /*btnHelp.setOnClickListener(v -> {
+                ttsManager.initQueue("Buen día ¿En qué le puedo ayudar?");
+                Intent sig = new Intent(MainActivity.this, Option_Accion.class);
+                startActivity(sig);
+            });*/
         }
 
     }
